1700f5a550a926fac448b48617c39ae4e3513e9b
Bhupendrashahu <76242175+Bhupendrashahu@users.noreply.github.com>
2021-05-18 18:44:40 +0530

    Add files via upload

---
+++ evenoddfunction.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void cube()
+{
+	int a;
+	cout <<"enter the no \n";
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<a<<"no is even";
+	}
+	else
+	{
+		cout<<"no is odd";
+	}
+	
+}
+int main()
+{
+ cube();
+}
